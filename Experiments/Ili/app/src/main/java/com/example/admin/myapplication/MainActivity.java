8ba0c76de8d4cbd8b01ba512fea7d1de3c9a4e78
illiand <illiand@iastate.edu>
2018-09-07 23:35:34 +0000

    Revert "Revert "can be moved by touch""
    This reverts commit c25ec39ee50d2e7ad839eb8c06c89ab20dd2e419

--- Experiments/Ili/app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ Experiments/Ili/app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.admin.myapplication;
 
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.media.Image;
+import android.media.JetPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Timer;
@@ -19,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Button right;
 
     private TextView tf;
+    private TextView consulor;
 
     private thread timer_CountDown;
 
@@ -30,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         right = findViewById(R.id.button_right);
 
         tf = findViewById(R.id.test);
+        consulor = findViewById(R.id.consulor);
 
         ActionListener al = new ActionListener();
 
@@ -39,7 +48,19 @@ public class MainActivity extends AppCompatActivity {
         right.setOnTouchListener(al);
 
         timer_CountDown = new thread(new Timer());
-        timer_CountDown.run();
+        ImageView JPanel1 = findViewById(R.id.image1);
+        ImageView JPanel2 = findViewById(R.id.image2);
+        JPanel1.setColorFilter(Color.rgb(50,100,150));
+        JPanel2.setColorFilter(Color.GREEN);
+    //    JPanel.setBackgroundColor(Color.rgb(50,100,150));
+    //    JPanel.getDrawingRect(new Rect(10,10,30,30));
+
+        MoveListener ml = new MoveListener();
+
+        JPanel1.setOnTouchListener(ml);
+        JPanel2.setOnTouchListener(ml);
+
+        JPanel1.setAlpha((float)1);
     }
 
     private class ActionListener implements View.OnTouchListener
@@ -50,24 +71,70 @@ public class MainActivity extends AppCompatActivity {
             switch (v.getId())
             {
                 case R.id.button_up:
-                    tf.setText("up" +  timer_CountDown.getRunningTime());
-                    return true;
+                    if(event.getAction() == MotionEvent.ACTION_DOWN)
+                    {
+                        timer_CountDown.run();
+                        consulor.setText("pressed ");
+                    }
+
+                    tf.setText("up " +  timer_CountDown.getRunningTime());
+
+                    if(event.getAction() == MotionEvent.ACTION_UP)
+                    {
+                        timer_CountDown.interrupt();
+                        consulor.setText("interupted");
+                    }
+
+                    break;
 
                 case R.id.button_down:
                     tf.setText("down"+  timer_CountDown.getRunningTime());
-                    return true;
+                    break;
 
                 case R.id.button_left:
                     tf.setText("left"+  timer_CountDown.getRunningTime());
-                    return true;
+                    break;
 
                 case R.id.button_right:
                     tf.setText("right"+  timer_CountDown.getRunningTime());
-                    return true;
-
-                default:
-                    return false;
+                    break;
             }
+
+            return true;
+        }
+    }
+
+    private class MoveListener implements View.OnTouchListener
+    {
+        private float oldX;
+        private float oldY;
+        private float iniX;
+        private float iniY;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event)
+        {
+            if(event.getAction() == MotionEvent.ACTION_DOWN)
+            {
+                int[] arr = new int[2];
+                v.getLocationOnScreen(arr);
+
+                oldX = v.getX() + event.getX();
+                oldY = v.getY() + event.getY();
+                iniX = v.getX();
+                iniY = v.getY();
+            }
+
+            float newX = v.getX() + event.getX();
+            float newY = v.getY() + event.getY();
+
+            float x = iniX + (newX - oldX);
+            float y = iniY + (newY - oldY);
+
+            v.setX(x);
+            v.setY(y);
+
+            return true;
         }
     }
 
@@ -76,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run()
         {
-
+            consulor.setText("Start");
         }
     }
 
@@ -94,6 +161,11 @@ public class MainActivity extends AppCompatActivity {
             super.run();
 
             runningTime = System.currentTimeMillis();
+        }
+
+        public void interrupt()
+        {
+            super.interrupt();
         }
 
         public long getRunningTime()
