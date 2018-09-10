1213ff88630cdb9f4079f8a5f083e1b73058aa55
Bansho <banshofk@iastate.edu>
2018-09-10 13:33:37 -0500

    Update calendar test

--- Experiments/Bansho/app/src/main/java/com/example/bansho/test/MainActivity.java
+++ Experiments/Bansho/app/src/main/java/com/example/bansho/test/MainActivity.java
@@ -1,36 +1,80 @@
 package com.example.bansho.test;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.GridView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1,b2;
+
+    private TextView titleText;
+    private Button prevButton, nextButton;
+    private CalendarAdapter mCalendarAdapter;
+    private GridView calendarGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1 =  findViewById(R.id.button_dont);
-        b2 =  findViewById(R.id.button_reset);
-
-        b1.setOnClickListener(new View.OnClickListener() {
+        titleText = findViewById(R.id.titleText);
+        prevButton = findViewById(R.id.prevButton);
+        prevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Don't Touch me", Toast.LENGTH_LONG).show();
+                mCalendarAdapter.prevMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
             }
         });
-
-        b2 .setOnClickListener(new View.OnClickListener() {
+        nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(i);
+                mCalendarAdapter.nextMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
             }
         });
+        calendarGridView = findViewById(R.id.calendarGridView);
+        mCalendarAdapter = new CalendarAdapter(this);
+        calendarGridView.setAdapter(mCalendarAdapter);
+        titleText.setText(mCalendarAdapter.getTitle());
     }
+
 }
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.Toast;
+//
+//public class MainActivity extends AppCompatActivity {
+//    Button b1,b2;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        b1 =  findViewById(R.id.button_dont);
+//        b2 =  findViewById(R.id.button_reset);
+//
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(),"Don't Touch me", Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        b2 .setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+//                startActivity(i);
+//            }
+//        });
+//    }
+//}
