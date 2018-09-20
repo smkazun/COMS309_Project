ad69212b0e3b105a17880dae49d61225ba4fbf82
Bansho <banshofk@iastate.edu>
2018-09-19 20:10:36 -0500

    update

--- Experiments/Bansho/app/src/main/java/com/example/bansho/test/MainActivity.java
+++ Experiments/Bansho/app/src/main/java/com/example/bansho/test/MainActivity.java
@@ -1,25 +1,30 @@
 package com.example.bansho.test;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView titleText;
-    private Button prevButton, nextButton;
+    private Button prevButton, nextButton, back,list;
     private CalendarAdapter mCalendarAdapter;
     private GridView calendarGridView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        back =  findViewById(R.id.back);
+        list =  findViewById(R.id.list);
         titleText = findViewById(R.id.titleText);
         prevButton = findViewById(R.id.prevButton);
         prevButton.setOnClickListener(new View.OnClickListener() {
@@ -41,16 +46,27 @@ public class MainActivity extends AppCompatActivity {
         mCalendarAdapter = new CalendarAdapter(this);
         calendarGridView.setAdapter(mCalendarAdapter);
         titleText.setText(mCalendarAdapter.getTitle());
+
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Don't Touch me", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
 }
-//import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.Toast;
-//
+
+
 //public class MainActivity extends AppCompatActivity {
 //    Button b1,b2;
 //
