8d56647ab836d6b63b23a0aa2251ff8ff0372f1a
Sebastian Kazun <smkazun@iastate.edu>
2018-09-23 14:01:27 -0500

    sql connects with local db; small fixes needed

--- Experiments/calenderAPI/src/main/java/com/example/demo/user/UserController.java
+++ Experiments/calenderAPI/src/main/java/com/example/demo/user/UserController.java
@@ -4,16 +4,42 @@ import java.util.List;
 import javax.persistence.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.user.Users;
+import com.example.demo.user.UserRepository;
 
 
 @RestController
+@RequestMapping(path = "/demo")
 public class UserController {
 	
 	@Autowired
-	UserRepository repository;
+	private UserRepository userRepository;
 	
-	
+	@GetMapping(path="/add") // Map ONLY GET Requests
+	public @ResponseBody String addNewUser (@RequestParam String name
+			, @RequestParam String email, @RequestParam String userType) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+
+		Users n = new Users();
+		n.setName(name);
+		n.setEmail(email);
+		n.setUserType(userType);
+		userRepository.save(n);
+		return "Saved";
+	}
+
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<Users> getAllUsers() {
+		// This returns a JSON or XML with the users
+		return userRepository.findAll();
+	}
 	
 	
 
--- Experiments/calenderAPI/src/main/java/com/example/demo/user/UserRepository.java
+++ Experiments/calenderAPI/src/main/java/com/example/demo/user/UserRepository.java
@@ -1,14 +1,17 @@
 package com.example.demo.user;
 
-import java.util.List;
+//import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
-
+import com.example.demo.user.Users;
 @Repository
-public interface UserRepository extends JpaRepository<Users, Integer>{
+public interface UserRepository extends CrudRepository<Users, Integer>{ //Crud
 
-		public List<Users> findAll();
+		//public List<Users> findAll();
+		
+		//List<Users> findById(@Param("id") int id);
 }
