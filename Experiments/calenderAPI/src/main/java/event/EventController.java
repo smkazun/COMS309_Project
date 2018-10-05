d5faa0709bf36427affc99f736c08ec0c9470ef0
Sebastian Kazun <smkazun@iastate.edu>
2018-10-05 12:00:53 -0500

    for events; still need to add some code

---
+++ Experiments/calenderAPI/src/main/java/event/EventController.java
@@ -0,0 +1,49 @@
+package event;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.user.UserRepository;
+import com.example.demo.user.Users;
+
+
+
+@RestController
+@RequestMapping(path = "/event")
+public class EventController {
+	
+	@Autowired
+	private EventRepository eventRepository;
+	
+	private final Logger logger = LoggerFactory.getLogger(EventController.class);
+	
+	//save new event
+	@RequestMapping(method = RequestMethod.POST, path = "/events/new")
+	public String saveEvent(Events event) {
+		//eventRepository.save(event);
+		return "New event saved";
+		
+	}
+	
+	//gets all events
+	@RequestMapping(method = RequestMethod.GET, path = "/events")
+	public List<Events> getAllEvents() {
+		logger.info("Entered into Controller layer");
+		//List<Events> results =  eventRepository.findAll();
+		//logger.info("number of records fetched: " + results.size());
+		//return results;
+		return null;
+	}
+		
+	
+	
+	
+}
