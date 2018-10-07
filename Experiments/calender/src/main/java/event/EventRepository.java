26ceb502af25f271352cb102dee30d7147543af7
James <jeenhuis@iastate.edu>
2018-10-07 15:13:11 -0500

    fixed and completed Event controller and repository to run

--- Experiments/calender/src/main/java/event/EventRepository.java
+++ Experiments/calender/src/main/java/event/EventRepository.java
@@ -3,10 +3,11 @@ package event;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.user.Users;
+//import com.example.demo.user.Users;
+import com.example.demo.user.event;
 
 @Repository
-public interface EventRepository extends CrudRepository<Users, Integer> {
+public interface EventRepository extends CrudRepository<event, Integer> {
 
 	//void save(Events event);
 
