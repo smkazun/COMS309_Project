d8376f25f9b5e3af35774c4a4e15dfda9f94c3e9
james <jeenhuis@iastate.edu>
2018-10-10 15:29:34 -0500

    add added accessCalendar table

--- Backend/calenderAPI/src/main/java/com/example/demo/accessCalendar/AccessCalendar.java
+++ Backend/calenderAPI/src/main/java/com/example/demo/accessCalendar/AccessCalendar.java
@@ -1,5 +1,52 @@
 package com.example.demo.accessCalendar;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+import org.springframework.core.style.ToStringCreator;
+
+@Entity
+@Table(name = "accessCalander")
 public class AccessCalendar {
+	
+	@Id
+	@Column(name = "ClientId")
+	@NotFound(action = NotFoundAction.IGNORE)
+	private Integer ClientId;
+	
+	@Id
+	@Column(name = "CalanderId")
+	@NotFound(action = NotFoundAction.IGNORE)
+	private Integer CalanderId;
+	
+	public Integer getClientID() {
+		return ClientId;
+	}
+	
+	public void setInstructorId(Integer id) {
+		this.ClientId = id;
+	}
+	
+	
+	public Integer getCalanderId() {
+		return CalanderId;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return new ToStringCreator(this)
+				
+				.append("CalanderId", this.getClientID())
+				.append("CalanderId", this.getCalanderId()).toString();
+		
+	}
 
 }
