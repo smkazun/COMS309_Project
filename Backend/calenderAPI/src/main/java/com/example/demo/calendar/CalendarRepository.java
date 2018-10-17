f2526bfaa685ddb8ae53ff3acc2d9abee95d7df8
James <jeenhuis@iastate.edu>
2018-10-17 15:25:08 -0500

    added the other primary key to accessCalendar table and added query to retreive all calendar names connected to a userId.

--- Backend/calenderAPI/src/main/java/com/example/demo/calendar/CalendarRepository.java
+++ Backend/calenderAPI/src/main/java/com/example/demo/calendar/CalendarRepository.java
@@ -1,9 +1,20 @@
 package com.example.demo.calendar;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.demo.user.Users;
 
 @Repository
 public interface CalendarRepository extends CrudRepository<Calendar, Integer> {
 
+	@Query(value ="select e.calendar_name " + 
+			"from calendar e, access_calander c" + 
+			"where e.calendar_id = c.calander_id " + 
+			"and c.client_id = 1?")
+	public List<Calendar> getCalendarNames(Integer userId);
+	
 }
