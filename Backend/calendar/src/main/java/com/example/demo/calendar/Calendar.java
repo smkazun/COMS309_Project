78f313cf84fae2259d2b50bc64573e4e60db6194
James <jeenhuis@iastate.edu>
2018-10-28 16:38:20 -0500

    modified users and calendar tables with many to many relations

--- Backend/calendar/src/main/java/com/example/demo/calendar/Calendar.java
+++ Backend/calendar/src/main/java/com/example/demo/calendar/Calendar.java
@@ -1,7 +1,17 @@
 package com.example.demo.calendar;
 
 import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -10,25 +20,29 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 import org.springframework.core.style.ToStringCreator;
 
+import com.example.demo.user.*;;
+
 @Entity
 @Table(name = "calendar")
 public class Calendar {
-	
+
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "calendarId")
-	@NotFound(action = NotFoundAction.IGNORE)
 	private Integer calendarId;
-	
-	@Column(name = "calendarName")
-	@NotFound(action = NotFoundAction.IGNORE)
+
 	private String calendarName;
 	
+	@JsonIgnore
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "AccessCalendar",
+			joinColumns = { @JoinColumn(name = "Calendar_id")},
+			inverseJoinColumns = { @JoinColumn(name = "User_id") })
+	private Set<Users> users = new HashSet<>();
 	
 	public Integer getCalendarId() {
 		return calendarId;
@@ -46,14 +60,28 @@ public class Calendar {
 		this.calendarName = calendarName;
 	}
 	
-	@Override
-	public String toString() {
-		return new ToStringCreator(this)
-				
-				.append("calendarId", this.getCalendarId())
-				.append("calendarName", this.getCalendarName()).toString();
-		
+	public Set<Users> getUsers(){
+		return users;
 	}
 	
-
+	public void setUsers(Set<Users> users) {
+		this.users = users;
+	}
+	
+	public Map<String, Object> toDTO()
+	{
+		Map<String,Object> map = new HashMap<>();
+		map.put("Calendar_id", this.calendarId);
+		
+		Set<Map<String,Object>> usersDTO = new HashSet<>();
+		for(Users u : users) {
+			Map<String,Object> usersMap = new HashMap<>();
+			usersMap.put("user_id", u.getId());
+			usersDTO.add(usersMap);
+		}
+		map.put("Users", usersDTO);
+		
+		return map;
+	}
+	
 }
--- Backend/calendar/src/main/java/com/example/demo/user/Users.java
+++ Backend/calendar/src/main/java/com/example/demo/user/Users.java
@@ -1,83 +1,81 @@
 package com.example.demo.user;
 
 import javax.persistence.Column;
+import java.util.HashSet;
+import java.util.Set;
 
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import com.example.demo.calendar.Calendar;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 import org.springframework.core.style.ToStringCreator;
 
 @Entity
-@Table(name = "users")
 public class Users {
-	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	@NotFound(action = NotFoundAction.IGNORE)
 	private Integer id;
 	
-	@Column(name = "name")
-	@NotFound(action = NotFoundAction.IGNORE)
 	private String name;
-	
-	@Column(name = "email")
-	@NotFound(action = NotFoundAction.IGNORE)
+
 	private String email;
-	
-	@Column(name = "user_type")
-	@NotFound(action = NotFoundAction.IGNORE)
+
 	private String userType; //private UserType userType;
 	
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "Users")
+	private Set<Calendar> calendars = new HashSet<>();
+    
+    	public Integer getId() {
+			return id;
+		}	
 	
-	public Integer getId() {
-		return id;
-	}
+		public void setId(Integer id) {
+			this.id = id;
+		}
 	
-	public void setId(Integer id) {
-		this.id = id;
-	}
+		public String getName() {
+			return name;
+		}
 	
-	public String getName() {
-		return name;
-	}
+		public void setName(String name) {
+			this.name = name;
+		}
 	
-	public void setName(String name) {
-		this.name = name;
-	}
+		public String getEmail() {
+			return this.email;
+		}
 	
-	public String getEmail() {
-		return this.email;
-	}
+		public void setEmail(String email) {
+			this.email = email;
+		}
 	
-	public void setEmail(String email) {
-		this.email = email;
-	}
+		public String getUserType() {
+			return this.userType;
+		}
 	
-	public String getUserType() {
-		return this.userType;
-	}
-	
-	public void setUserType(String userType) {
-		this.userType = userType;
-	}
-	
-	@Override
-	public String toString() {
-		return new ToStringCreator(this)
-				
-				.append("id", this.getId())
-				.append("name", this.getName())
-				.append("type", this.getUserType())
-				.append("email", this.getEmail()).toString();
+		public void setUserType(String userType) {
+			this.userType = userType;
+		}
 		
+		public Set<Calendar> getCalendars(){
+			return calendars;
+		}
 		
-	}
-	
+		public void setCalendars(Set<Calendar> calendars) {
+			this.calendars = calendars;
+		}
+		
 }
+	
