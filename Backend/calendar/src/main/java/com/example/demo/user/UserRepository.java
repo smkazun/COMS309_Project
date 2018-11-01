1391ca3e1639a2af1f4f22c82aa02f1007314af1
James <jeenhuis@iastate.edu>
2018-10-31 19:43:00 -0500

    removed overide methods in user repos

--- Backend/calendar/src/main/java/com/example/demo/user/UserRepository.java
+++ Backend/calendar/src/main/java/com/example/demo/user/UserRepository.java
@@ -12,18 +12,6 @@ import com.example.demo.user.Users;
 
 @Repository
 public interface UserRepository extends CrudRepository<Users, Integer>{
-
-		//public List<Users> findAll();
-		
-		//List<Users> findById(@Param("id") int id);
-	@Query(  value ="SELECT e \r\n" + 
-			"FROM users e, access_calander b\r\n" + 
-			"where e.id = b.client_id\r\n" + 
-			"and b.calander_id = ?1;", nativeQuery = true)
-	public List<Users> getAllNames(Integer id);
-
-	@Query( value = "select * from users;", nativeQuery = true)
-	public List<Users> getAllUsers();
 	
 	//finds user
 	public Optional<Users> findByuserid(Integer Userid);
