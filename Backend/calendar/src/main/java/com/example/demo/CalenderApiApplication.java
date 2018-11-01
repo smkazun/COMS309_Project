57f740520c75c6542c12b00fe7d5ebc37e0ad53c
James <jeenhuis@iastate.edu>
2018-11-01 15:48:42 -0500

    fixed websocket conflict

--- Backend/calendar/src/main/java/com/example/demo/CalenderApiApplication.java
+++ Backend/calendar/src/main/java/com/example/demo/CalenderApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"WebSocket"})
+@ComponentScan(basePackages = {"WebSocket","com.example.demo"})
 public class CalenderApiApplication {
 
 	public static void main(String[] args) {
